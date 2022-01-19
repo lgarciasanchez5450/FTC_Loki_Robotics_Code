4aea6e5a4ada2ac17731c69ae235470114a2e6f3
johno <johnolivas7553@gmail.com>
2022-01-18 17:16:55 -0800

    I made sum pi. and then I made it final

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAutonomous.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAutonomous.java
@@ -23,7 +23,7 @@ public class BasicAutonomous extends LinearOpMode {
 
 
     //private DcMotor duckMotor = null;
-    static double pi = Math.PI;
+    static final double pi = Math.PI;
     static final double COUNTS_PER_MOTOR_REV = 28;    // eg: TETRIX Motor Encoder
     static final double DRIVE_GEAR_REDUCTION = 20/2;     // This is < 1.0 if geared UP
     static final double WHEEL_DIAMETER_INCHES = 4;     // For figuring circumference
