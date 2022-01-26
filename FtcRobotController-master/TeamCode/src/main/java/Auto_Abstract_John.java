2ee6a2f6846e7a1dcb93873a9e043744c83363e4
Andy Smithwick <andy.smithwick@gmail.com>
2022-01-25 17:32:06 -0800

    This adds John's abstract auto 2

--- FtcRobotController-master/TeamCode/src/main/java/Auto_Abstract_John.java
+++ FtcRobotController-master/TeamCode/src/main/java/Auto_Abstract_John.java
@@ -3,8 +3,16 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 public abstract class Auto_Abstract_John extends LinearOpMode {
-    //These are the variables
-    public DcMotor lf,rf,rb,lb;
 
 
+    private final ElapsedTime runtime = new ElapsedTime();
+    private DcMotor lf = null;
+    private DcMotor rf = null;
+    private DcMotor lb = null;
+    private DcMotor rb = null;
+
+    static final double pi =Math.PI;
+    static final double countsPerRev = 28;
+    static final double driverGearReduction = 20;
+    static final double wheelDiameter = 3.97638;
 }
