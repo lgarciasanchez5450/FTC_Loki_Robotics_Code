c3e88093ce37884846f2a76cb28464dc9492b53b
Andy Smithwick <andy.smithwick@gmail.com>
2022-02-11 10:16:38 -0800

    Adjusted file locations, added state machine teleOp and HardwareBot
    Under FiniteStates_TeleOp and LokiHardwareBot

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/archive/Auto_Abstract_John.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/archive/Auto_Abstract_John.java
@@ -1,3 +1,5 @@
+package org.firstinspires.ftc.teamcode.archive;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
